ba015f9ddb5a35228dd2ac23c9eb78b10da86015
bbelderbos <bobbelderbos@gmail.com>
2017-03-08 14:59:06 +0100

    add authentication to get route

--- api.py
+++ api.py
@@ -1,20 +1,52 @@
 from collections import namedtuple
 from functools import wraps
 import os
-import sys
 import time
 
-from flask import Flask, abort, jsonify, make_response, request
+from flask import Flask, abort, jsonify, make_response, request, Response
 
 from backend import get_sheet, get_next_row, convert_time
 
 COMMAND = '/dw'
 SLACK_DW_CMD_TOKEN = os.environ.get('SLACK_DW_CMD_TOKEN')
+SLACK_DW_USER = os.environ.get('SLACK_DW_USER')
+SLACK_DW_PW = os.environ.get('SLACK_DW_PW')
 
 Entry = namedtuple('entry', 'user time seconds activity')
 
 sheet = get_sheet()
 app = Flask(__name__)
+
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    print(username)
+    print(password)
+    print(SLACK_DW_USER)
+    print(SLACK_DW_PW)
+    print( username == SLACK_DW_USER and password == SLACK_DW_PW)
+    return username == SLACK_DW_USER and password == SLACK_DW_PW
+
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+        'Could not verify your access level for that URL.\n'
+        'You have to login with proper credentials', 401,
+        {'WWW-Authenticate': 'Basic realm="Login Required"'}
+    )
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
 
 
 @app.errorhandler(400)
@@ -23,6 +55,7 @@ def not_found(error):
 
 
 @app.route('/api/v1.0/entries', methods=['GET'])
+@requires_auth
 def get_items():
     entries = sheet.get_all_records()
     return jsonify({'entries': entries})
